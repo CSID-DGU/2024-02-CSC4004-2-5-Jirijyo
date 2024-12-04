083ad4007bbb4d2a4ff22decdf2463aae40ce949
BaekMinGyeong <bmgbmk0423@gmail.com>
2024-12-04 11:25:31 +0900

    index.html 수정 및 start.html 추가
    이미지 원본과 결과를 보여주고, 다른 이미지로 다시 해보거나 이미지를 저장하는 버튼을 추가했습니다.
    프로젝트를 소개하는 페이지를 추가했습니다.

--- web/src/App.js
+++ web/src/App.js
@@ -3,6 +3,15 @@ document.addEventListener('DOMContentLoaded', () => {
   const fileInput = document.getElementById('file-upload');
   const resultContainer = document.getElementById('result-container');
 
+  //새로 추가
+  const actionButtons = document.querySelector('.action-buttons');
+  const retryButton = document.getElementById('retry-button');
+  const downloadButton = document.getElementById('download-button');
+
+  resultContainer.style.display = 'none';
+  actionButtons.style.display = 'none';
+  //
+
   uploadForm.addEventListener('submit', async (event) => {
     event.preventDefault(); // 기본 동작(페이지 새로고침) 방지
 
@@ -27,7 +36,29 @@ document.addEventListener('DOMContentLoaded', () => {
         const imageUrl = URL.createObjectURL(blob);
 
         // 결과 이미지 표시
-        resultContainer.innerHTML = `<img src="${imageUrl}" alt="Detection Result" class="result-image">`;
+        //resultContainer.innerHTML = `<img src="${imageUrl}" alt="Detection Result" class="result-image">`;
+
+        //추가 : 원본 이미지와 결과 이미지 표
+        resultContainer.innerHTML = `
+          <div id="original-image-container">
+            <h3>원본 이미지</h3>
+            <img id="original-image" src="${URL.createObjectURL(file)}" alt="Original Image" class="result-image">
+          </div>
+          <div id="result-image-container">
+            <h3>결과 이미지</h3>
+            <img id="result-image" src="${imageUrl}" alt="Detection Result" class="result-image">
+          </div>
+        `;
+       
+        resultContainer.style.display = 'block';
+
+         // "다시 하기"와 "다운로드" 버튼을 보이게 설정
+        actionButtons.style.display = 'block';
+
+        // 다운로드 버튼의 링크를 결과 이미지로 설정
+        downloadButton.href = imageUrl;
+        downloadButton.download = 'result-image.png';
+
       } else {
         const error = await response.json();
         alert(`오류 발생: ${error.error}`);
@@ -36,5 +67,12 @@ document.addEventListener('DOMContentLoaded', () => {
       console.error('Error uploading image:', error);
       alert('서버와의 통신 중 오류가 발생했습니다.');
     }
+  });
+  retryButton.addEventListener('click', () => {
+    // 업로드 폼과 결과 초기화
+    fileInput.value = ''; // 파일 선택 초기화
+    resultContainer.style.display = 'none';
+    actionButtons.style.display = 'none';
+    resultContainer.innerHTML = ''; // 결과 컨테이너 비우기
   });
 });
