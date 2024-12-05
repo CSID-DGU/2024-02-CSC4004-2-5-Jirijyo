45c7b589e4e36aa0251fac847792f2a44ca6fa57
이용우 <qhtm008@naver.com>
2024-12-05 17:37:07 +0900

    Demo

--- web/src/App.js
+++ web/src/App.js
@@ -2,6 +2,7 @@ document.addEventListener('DOMContentLoaded', () => {
   const uploadForm = document.getElementById('upload-form');
   const fileInput = document.getElementById('file-upload');
   const resultContainer = document.getElementById('result-container');
+  const originalContainer = document.getElementById('original-container');
 
   uploadForm.addEventListener('submit', async (event) => {
     event.preventDefault(); // 기본 동작(페이지 새로고침) 방지
@@ -11,10 +12,18 @@ document.addEventListener('DOMContentLoaded', () => {
       alert('이미지를 선택하세요.');
       return;
     }
+    const imageUrl = URL.createObjectURL(file); // 업로드된 파일을 임시 URL로 변환
+    originalContainer.innerHTML = `<img src="${imageUrl}" alt="Original Source" class="original-image">`;
+
+    if (!document.querySelector('.dynamic-arrow')) {
+      const arrow = document.createElement('div');
+      arrow.classList.add('arrow', 'dynamic-arrow'); // 화살표 클래스 추가
+      arrow.innerHTML = '→';
+      originalContainer.parentElement.insertBefore(arrow, resultContainer); // 화살표를 두 컨테이너 사이에 삽입
+    }
 
     const formData = new FormData();
     formData.append('image', file);
-
     try {
       // Flask 서버에 POST 요청
       const response = await fetch('http://127.0.0.1:5000/process-image', {
@@ -27,6 +36,7 @@ document.addEventListener('DOMContentLoaded', () => {
         const imageUrl = URL.createObjectURL(blob);
 
         // 결과 이미지 표시
+        
         resultContainer.innerHTML = `<img src="${imageUrl}" alt="Detection Result" class="result-image">`;
       } else {
         const error = await response.json();
